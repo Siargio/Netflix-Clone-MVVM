1e7592443451312fc11534c02a0092a658e167a9
Siarheimasiuk <winst73@gmail.com>
2022-12-27 17:07:14 +0300

    add gitignore, clean code

---
+++ Netflix-Clone-MVVM/App/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Netflix-Clone-MVVM
+//
+//  Created by Sergio on 27.12.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
